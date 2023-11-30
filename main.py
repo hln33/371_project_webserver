64c4173c7eb06580c28e3ca0ddda4f968330b213
hln5 <hln5@sfu.ca>
2023-11-29 19:54:57 -0800

    able to handle get request for test.html

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from socket import *
+import os
 
 
-def handle_request(request_data):
-    print(request_data)
+def get_file_content(file_path):
+    if os.path.isfile(file_path):
+        with open(file_path, 'rb') as file:
+            content = file.read()
+            print(content)
+    else:
+        print('error')
+
+
+def handle_request(http_request: str):
+    lines = http_request.split('\r\n')
+
+    # first line in http request is the request line
+    request_line = lines[0]
+    request_type = request_line.split()[0]
+    file_path = os.getcwd() + request_line.split()[1]
+    get_file_content(file_path)
 
 
 def run_server(ip_addr: str, port: int):
@@ -12,11 +28,12 @@ def run_server(ip_addr: str, port: int):
 
     while True:
         client_socket, client_address = server_socket.accept()
-        request_data = client_socket.recvfrom(1024)
+        request_data = client_socket.recvfrom(1024)[0].decode('ascii')
         handle_request(request_data)
+        client_socket.close()
 
 
 if __name__ == '__main__':
     IP_ADDR = 'localhost'
-    PORT_NUMBER = 8000
+    PORT_NUMBER = 8005
     run_server(IP_ADDR, PORT_NUMBER)
