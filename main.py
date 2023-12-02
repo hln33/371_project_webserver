b2d0b96424defc648a4f26e5a84f2372cc65f013
hln5 <hln5@sfu.ca>
2023-12-01 18:13:51 -0800

    implement 411 error response

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import os
 OK_CODE = '200 Ok'
 NOT_MODIFIED_CODE = '304 Not Modified'
 BAD_REQUEST_CODE = '400 Bad Request'
-UNAUTHORIZED_CODE = '401 Unauthorized'
+UNAUTHORIZED_CODE = '403 Forbidden'
 NOT_FOUND_CODE = '404 Not Found'
+LENGTH_REQUIRED_CODE = '411 Length Required'
 
 
 def get_file_content(file_path):
@@ -24,6 +25,8 @@ def construct_http_response(status_line: str, data: str) -> bytes:
 
 
 def handle_request(http_request: str):
+    print(http_request)
+
     lines = http_request.split('\r\n')
 
     # first line in http request is the request line
@@ -31,23 +34,33 @@ def handle_request(http_request: str):
     request_type = request_line.split()[0]
     file_name = request_line.split()[1]
     file_path = os.getcwd() + file_name
+    content = get_file_content(file_path)
 
     status_line = BAD_REQUEST_CODE
     data = BAD_REQUEST_CODE
     if request_type == 'GET':
-        content = get_file_content(file_path)
         if 'If-Modified-Since' in http_request:
             status_line = NOT_MODIFIED_CODE
             data = NOT_MODIFIED_CODE
-        elif file_name == 'test_auth' and 'Authorization' not in http_request:
+        elif file_name == '/test_auth.html' and 'Authorization' not in http_request:
             status_line = UNAUTHORIZED_CODE
             data = UNAUTHORIZED_CODE
+        elif file_name == '/test_content_len_req' and 'Content-Length' not in http_request:
+            status_line = LENGTH_REQUIRED_CODE
+            data = LENGTH_REQUIRED_CODE
         elif content:
             status_line = OK_CODE
             data = content.decode('utf-8')
         else:
             status_line = NOT_FOUND_CODE
             data = NOT_FOUND_CODE
+    elif request_type == 'POST':
+        if file_name == '/test_content_len_req.html' and 'Content-Length' not in http_request:
+            status_line = LENGTH_REQUIRED_CODE
+            data = LENGTH_REQUIRED_CODE
+        elif content:
+            status_line = OK_CODE
+            data = content.decode('utf-8')
     return construct_http_response(status_line, data)
 
 
@@ -58,6 +71,7 @@ def run_server(ip_addr: str, port: int):
 
     while True:
         client_socket, client_address = server_socket.accept()
+        print(client_address)
         request_data = client_socket.recvfrom(1024)[0].decode('ascii')
         response = handle_request(request_data)
         client_socket.send(response)
@@ -66,5 +80,5 @@ def run_server(ip_addr: str, port: int):
 
 if __name__ == '__main__':
     IP_ADDR = 'localhost'
-    PORT_NUMBER = 8000
+    PORT_NUMBER = 8001
     run_server(IP_ADDR, PORT_NUMBER)
